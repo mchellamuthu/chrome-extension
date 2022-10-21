adddc6a97c1ebaed234e54aabce8cd2b33b3b316
Chella <mchellamuthu@live.com>
2022-10-21 16:44:00 +0530

    Initial

---
+++ src/extension.js
@@ -0,0 +1,36 @@
+"use strict";
+
+// loader-code: wait until gmailjs has finished loading, before triggering actual extensiode-code.
+const loaderId = setInterval(() => {
+  if (!window._gmailjs) {
+    return;
+  }
+
+  clearInterval(loaderId);
+  getTitle();
+  startExtension(window._gmailjs);
+}, 100);
+
+function startExtension(gmail) {
+  console.log("Extension loading...");
+  window.gmail = gmail;
+
+  gmail.observe.on("load", () => {
+    const userEmail = gmail.get.user_email();
+    console.log("Hello, " + userEmail + ". This is your extension talking!");
+
+    gmail.observe.on("view_email", (domEmail) => {
+      console.log("Looking at email:", domEmail);
+      const emailData = gmail.new.get.email_data(domEmail);
+      console.log("Email data:", emailData.subject);
+    });
+
+    gmail.observe.on("compose", (compose) => {
+      console.log("New compose window is opened!", compose);
+    });
+  });
+}
+
+function getTitle() {
+  console.log("Title: " + document.title);
+}
---
+++ src/gmailJsLoader.js
@@ -0,0 +1,7 @@
+"use strict";
+
+
+const GmailFactory = require("gmail-js");
+const jQuery = require("jquery");
+
+window._gmailjs = window._gmailjs || new GmailFactory.Gmail(jQuery);
